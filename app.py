f9d01fda5dfcfae159ccd6ddcbfb71f1a0153769
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2024-04-12 07:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template,request,jsonify
 from pymongo import MongoClient
-from bot import telegram
 import re,os
 
 
@@ -8,18 +7,18 @@ uri = os.environ.get('URI')
 client = MongoClient(uri)
 db = client['database']
 collection = db['data']
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 dwn_url = os.environ.get('STREAM_URL')
 
 
 @app.get('/')
 def index ():
     items = list(collection.find())
-    return render_template('index.html',items=items[::-1])
+    return render_template('home.html',items=items[::-1])
 @app.get('/<id>/<movie>')
 def page(id,movie):
     item= collection.find_one({"_id":int(id)})
-    return render_template('page.html',item=item,username=os.environ.get('BOT_USERNAME'),dwn_url=dwn_url)
+    return render_template('page.html',item=item,dwn_url=dwn_url)
 
 @app.get('/search')
 def search():
@@ -31,12 +30,24 @@ def search():
     else:
         return jsonify(items)
 
-@app.post('/telegram')
-def tg():
-    data = request.get_json()
-    if data.get('message'):
-        telegram(data)
-    return jsonify(data)
+
+@app.route('/stream')
+def stream():
+    id = request.args.get('id')
+    item = collection.find_one({"_id":int(id)})
+    return render_template('stream.html',dwn_url=dwn_url,item=item,size_format=size_format)
+
+def size_format(b):
+    if b < 1000:
+              return '%i' % b + 'B'
+    elif 1000 <= b < 1000000:
+        return '%.1f' % float(b/1000) + 'KB'
+    elif 1000000 <= b < 1000000000:
+        return '%.1f' % float(b/1000000) + 'MB'
+    elif 1000000000 <= b < 1000000000000:
+        return '%.1f' % float(b/1000000000) + 'GB'
+    elif 1000000000000 <= b:
+        return '%.1f' % float(b/1000000000000) + 'TB'
 
 if __name__ == '__main__':
     app.run()
